2332badba1e7f649d121a0737233e4ffc536e18e
KalidassG <136417433+KalidassG@users.noreply.github.com>
2023-10-17 09:08:26 +0530

    Update myprogram.java

--- myprogram.java
+++ myprogram.java
@@ -1,8 +1,6 @@
-public class HelloWorld {
+public class Main {
 
-  public static void main(String[] args) {
-    System.out.println("Hello, world!");
-  }
-
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
 }
-
